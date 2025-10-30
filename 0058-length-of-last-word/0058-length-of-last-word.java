89e3679e6649bd1a770f7ef7a25f8ac238603819
Elkhazri Hajar <151858228+hajarelkhazri@users.noreply.github.com>
2025-10-30 10:22:54 +0100

    Time: 0 ms (100%), Space: 41.9 MB (32.3%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,11 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        if(s = null){
-            return 0;
-        }
+        // if(s = null){
+        //     return 0;
+        // }
+        int count =0;
         for(int i=s.length()-1;i>=0;i--){
-
+            if(s.charAt(i) != ' '){
+                count++;
+            }
+            else{
+                if (count > 0){
+                    return count;
+                }
+            }
         }
-        
+        return count;
+
     }
 }
